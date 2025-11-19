59685953f18b99c696daef4f53fefeb50734462e
adamel <elbadamx@gamil.com>
2025-11-20 00:09:10 +0100

    lagt til firebaseEnv

--- firebase.js
+++ firebase.js
@@ -9,6 +9,7 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import firebaseConfig from "./firebaseEnv"
 
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
@@ -16,16 +17,6 @@ GoogleSignin.configure({
   webClientId: "520017838610-domr1fpg6le07e4hku5bmvafogahts1s.apps.googleusercontent.com",
 });
 
-
-// firebase config (bruk din eksisterende)
-const firebaseConfig = {
-  apiKey: "AIzaSyA81IIHcX6ZCONRAifIj1ESpCwfjS5ZB8w",
-  authDomain: "dugnadhub-5e674.firebaseapp.com",
-  projectId: "dugnadhub-5e674",
-  storageBucket: "dugnadhub-5e674.firebasestorage.app",
-  messagingSenderId: "520017838610",
-  appId: "1:520017838610:web:8892320d7ce73e9ee0cecb",
-};
 
 // initialiser selve Firebase-appen
 const app = initializeApp(firebaseConfig);
---
+++ firebaseEnv.js
@@ -0,0 +1,11 @@
+// firebase config (bruk din eksisterende)
+const firebaseConfig = {
+    apiKey: "AIzaSyA81IIHcX6ZCONRAifIj1ESpCwfjS5ZB8w",
+    authDomain: "dugnadhub-5e674.firebaseapp.com",
+    projectId: "dugnadhub-5e674",
+    storageBucket: "dugnadhub-5e674.firebasestorage.app",
+    messagingSenderId: "520017838610",
+    appId: "1:520017838610:web:8892320d7ce73e9ee0cecb",
+  };
+
+  export default firebaseConfig;
